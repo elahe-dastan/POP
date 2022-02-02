ba907d985446ce3aaed122a70e1df20834bc64cc
Elahe Dastan <elahe.dstn@gmail.com>
2022-02-02 21:05:02 +0330

    elastic connection

---
+++ connections/elastic.py
@@ -0,0 +1,35 @@
+from elasticsearch import Elasticsearch
+
+
+class Elastic:
+    """ElasticSearch connection"""
+
+    _host = None
+    _port = None
+    _user = None
+    _password = None
+    _client = None
+
+    def __init__(self, host: str, port: int = 443, user: str = "", password: str = ""):
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+
+    def connect(self) -> Elasticsearch:
+        self._client = Elasticsearch(hosts=[self._host + ":" + str(self._port)],
+                                     http_auth=(self._user, self._password),
+                                     timeout=300)
+        return self._client
+
+    def search(self, index: str, body: dict):
+        client = self.connect()
+        response = client.search(
+            index=index,
+            body=body
+        )
+
+        return response
+
+    def close(self) -> None:
+        self._client.transport.close()
