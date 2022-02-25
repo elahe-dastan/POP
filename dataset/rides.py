dfdd1457bf2a0844221ad55a7ba94e89370164e0
raha <elahe.dstn@gmail.com>
2022-02-26 00:51:37 +0330

    elastic

--- dataset/rides.py
+++ dataset/rides.py
@@ -1,12 +1,16 @@
 class RideInfo:
-    def __init__(self, hit):
+    def __init__(self, hit, le):
         hit = hit['_source']
-        self.eta = hit['eta_comp_result']['smapp']
+        self.eta = int(hit['eta_comp_result']['smapp'])
 
-        self.iran_day_of_week = hit['iran_day_of_week']
-        self.iran_time_of_day = hit['iran_time_of_day']
+        self.iran_day_of_week = le[hit['iran_day_of_week']]
 
-        self.ata = hit['ata_duration']
+        self.hour = hit['iran_time_of_day'].split(":")[0]
+        self.minute = hit['iran_time_of_day'].split(":")[1]
+        self.iran_time_of_day = int(self.hour) + int(self.minute) / 60
+
+        self.ata = int(hit['ata_duration'])
+        self.confidence = float(hit['confidence'])
 
         hit = hit['ride']
         self.origin_lat = hit['origin']['lat']
@@ -17,11 +21,11 @@ class RideInfo:
 
 
 class Rides:
-    def __init__(self, res):
+    def __init__(self, res, le):
         res = res['hits']['hits']
         self.hits = []
         for hit in res:
-            self.hits.append(RideInfo(hit))
+            self.hits.append(RideInfo(hit, le))
 
 
 class RideDataset:
@@ -29,6 +33,14 @@ class RideDataset:
         # Elastic search server is configured to return 10000 results at most
         self.MAX_HIT = 10000
         self._elastic_connection = elastic_connection
+        self.le = {"SATURDAY": 0,
+                   "SUNDAY": 1,
+                   "MONDAY": 2,
+                   "TUESDAY": 3,
+                   "WEDNESDAY": 4,
+                   "THURSDAY": 5,
+                   "FRIDAY": 6
+                   }
 
     def fetch_rides(self, time_least, time_most):
         index = "farsanj-results-*"
@@ -44,10 +56,15 @@ class RideDataset:
                                     "lte": time_most
                                 }
                             }
+                        },
+                        {
+                            "term": {
+                                "origin_city_id": 1
+                            }
                         }
                     ]
                 }
             }
         })
 
-        return Rides(res)
+        return Rides(res, self.le).hits
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
-from torch.utils.data import DataLoader
+# from torch.utils.data import DataLoader
 # from torchvision import transforms
-import pytorch_lightning as pl
+# import pytorch_lightning as pl
 
-from MLP import MLP
+# from MLP import MLP
 from connections.elastic import Elastic
-from rides import RideDataset
+from dataset.rides import RideDataset
 
 if __name__ == '__main__':
     elastic_connection = Elastic(host='https://routing-elasticsearch.apps.private.teh-1.snappcloud.io')
     rideDataset = RideDataset(elastic_connection=elastic_connection)
     rides = rideDataset.fetch_rides(time_least=1644610058, time_most=1644613658)
-    print(rides.hits)
+    print(rides)
     # dataset = CIFAR10(os.getcwd(), download=True, transform=transforms.ToTensor())
     # pl.seed_everything(42)
     # mlp = MLP()
