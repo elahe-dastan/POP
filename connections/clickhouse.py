1f8408090c5b6ccf929a5075b0d95c26cc1fbed1
Elahe Dastan <elahe.dastan@snapp.cab>
2022-10-02 21:55:56 +0330

    normalize data

--- connections/clickhouse.py
+++ connections/clickhouse.py
@@ -5,17 +5,21 @@ client = Client(host='172.21.16.1',
                 user='map_user',
                 password='6zeQUqCj4y7puMxbBCeN')
 
-# a = client.execute('DESCRIBE TABLE snapp_raw_log.khatkesh_result_proto')
-# print(a)
+a = client.execute('DESCRIBE TABLE snapp_raw_log.khatkesh_result_proto')
+print(a)
+print(len(a))
 #
 # b = client.execute('SELECT * FROM snapp_raw_log.khatkesh_result_proto LIMIT 1')
 # print(b)
 
-c = client.execute("SELECT * FROM snapp_raw_log.khatkesh_result_proto WHERE toDate(clickhouse_time, 'Iran') BETWEEN '2022-09-14' AND '2022-09-14' AND toHour(clickhouse_time, 'Iran') BETWEEN 17 AND 19")
+# c = client.execute("SELECT * FROM snapp_raw_log.khatkesh_result_proto WHERE toDate(clickhouse_time, 'Iran') BETWEEN '2022-09-14' AND '2022-09-14' AND toHour(clickhouse_time, 'Iran') BETWEEN 17 AND 19")
 
-with open('../khatkesh.csv', 'w') as f:
-    # using csv.writer method from CSV package
-    write = csv.writer(f)
+# with open('../khatkesh.csv', 'w') as f:
+#     # using csv.writer method from CSV package
+#     write = csv.writer(f)
+#
+#     # write.writerow(fields)
+#     write.writerows(c)
 
-    # write.writerow(fields)
-    write.writerows(c)
+b = ['ride_id','driver_id','a_t_a_result.arrival_a_t_a','a_t_a_result.boarding_a_t_a','a_t_a_result.ride_a_t_a','a_t_a_result.arrival_probe_result.probe.point.lat','a_t_a_result.arrival_probe_result.probe.point.lon','a_t_a_result.arrival_probe_result.probe.timestamp','a_t_a_result.arrival_probe_result.confidence','a_t_a_result.arrival_probe_result.h3_index', 'a_t_a_result.arrival_probe_result.k_ring_level','a_t_a_result.boarding_probe_result.probe.point.lat','a_t_a_result.boarding_probe_result.probe.point.lon','a_t_a_result.boarding_probe_result.probe.timestamp','a_t_a_result.boarding_probe_result.confidence','a_t_a_result.boarding_probe_result.h3_index','a_t_a_result.boarding_probe_result.k_ring_level','a_t_a_result.final_destination_probe_result.probe.point.lat','a_t_a_result.final_destination_probe_result.probe.point.lon','a_t_a_result.final_destination_probe_result.probe.timestamp','a_t_a_result.final_destination_probe_result.confidence','a_t_a_result.final_destination_probe_result.h3_index','a_t_a_result.final_destination_probe_result.k_ring_level','a_t_a_result.destination_probe_result.probe.point.lat','a_t_a_result.destination_probe_result.probe.point.lon','a_t_a_result.destination_probe_result.probe.timestamp','a_t_a_result.destination_probe_result.confidence','a_t_a_result.destination_probe_result.h3_index','a_t_a_result.destination_probe_result.k_ring_level','a_t_a_result.extra_destination_probe_result.probe.point.lat','a_t_a_result.extra_destination_probe_result.probe.point.lon','a_t_a_result.extra_destination_probe_result.probe.timestamp','a_t_a_result.extra_destination_probe_result.confidence','a_t_a_result.extra_destination_probe_result.h3_index','a_t_a_result.extra_destination_probe_result.k_ring_level','pickup_a_d_d_result.distance','pickup_a_d_d_result.confidence','pickup_a_d_d_result.route_ratio','pickup_a_d_d_result.g_p_s_ratio','ride_a_d_d_result.distance','ride_a_d_d_result.confidence','ride_a_d_d_result.route_ratio','ride_a_d_d_result.g_p_s_ratio','total_a_d_d_confidence','in_ride_allotment','e_d_d','clickhouse_time','hash']
+print(len(b))
